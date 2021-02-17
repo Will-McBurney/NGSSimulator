e1d0147d1a110907c3fc27cb48d40279426bd317
Will McBurney <pmcburn1@gmail.com>
2021-02-17 12:57:15 -0500

    Took out script file

--- runsim.ps1
+++
@@ -1,8 +0,0 @@
-python schedule_getter.py
-rm data\schedules\Schedules.xlsx
-mv Schedules.xlsx data\schedules\Schedules.xlsx
-rm simulationResults.txt
-java -jar NGSSimulator.jar all > simulationResults.txt
-git add simulationResults.txt
-git commit -m "Batch Simulation Results commit"
-git push
